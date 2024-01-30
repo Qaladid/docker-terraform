2ed3e8d19eb18ffb2bca520569b52b859508f230
qaladid <abdiaziz11410@gmail.com>
2024-01-30 21:20:51 +0300

    updated terraform directory, added .gitignore file for privacy

--- terraform_with_variables/main.tf
+++ terraform_with_variables/main.tf
@@ -14,7 +14,7 @@ provider "google" {
 }
 
 
-resource "google_storage_bucket" "demo-bucket" {
+resource "google_storage_bucket" "demo-bucket-hwork1014" {
   name          = var.gcs_bucket_name
   location      = var.location
   force_destroy = true
@@ -32,7 +32,7 @@ resource "google_storage_bucket" "demo-bucket" {
 
 
 
-resource "google_bigquery_dataset" "demo_dataset" {
+resource "google_bigquery_dataset" "demo_dataset-hwork1014" {
   dataset_id = var.bq_dataset_name
   location   = var.location
 }
